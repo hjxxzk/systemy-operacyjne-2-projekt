d1bb5476e024b3ec66b749a02d20eb3b8d4d649f
agnieszka <273017@student.pwr.edu.pl>
2025-03-15 20:54:41 +0100

    add forks id to philosopher struct

--- main.cpp
+++ main.cpp
@@ -4,24 +4,38 @@
 #include <iostream>
 
 pthread_mutex_t writeMutex = PTHREAD_MUTEX_INITIALIZER;
+int NUMBER_OF_PHILOSOPHERS;
+int INDEX_OF_FIRST_FORK = 0;
 pthread_mutex_t* forks;
 
 struct Philosopher {
     int id;
+    int leftForkId;
+    int rightForkId;
 };
 
 void* think(void *arg) {
     auto* philosopher = static_cast<Philosopher*>(arg);
 
     pthread_mutex_lock(&writeMutex);
-    std::cout << "Philosopher " << philosopher->id
-        << " - " << "thinking..." << std::endl;
+    std::cout << "Philosopher " << philosopher->id + 1
+        << " - " << "thinking... - left fork id: " << philosopher->leftForkId
+        << " right fork id: "<< philosopher->rightForkId << std::endl;
     pthread_mutex_unlock(&writeMutex);
     return nullptr;
 }
 
+int calculateRightForkId(int philosopherId) {
+    int indexOfLastPhilosopher = NUMBER_OF_PHILOSOPHERS - 1;
+    if (philosopherId == indexOfLastPhilosopher) {
+        return INDEX_OF_FIRST_FORK;
+    } else {
+        return philosopherId + 1;
+    }
+}
+
 int main(int argc, char* argv[]) {
-    const int NUMBER_OF_PHILOSOPHERS = std::stoi(argv[1]);
+    NUMBER_OF_PHILOSOPHERS = std::stoi(argv[1]);
     pthread_t threads[NUMBER_OF_PHILOSOPHERS];
     Philosopher philosophers[NUMBER_OF_PHILOSOPHERS];
     forks = new pthread_mutex_t[NUMBER_OF_PHILOSOPHERS];
@@ -31,7 +45,9 @@ int main(int argc, char* argv[]) {
     }
 
     for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
-        philosophers[i].id = i + 1;
+        philosophers[i].id = i;
+        philosophers[i].leftForkId = i;
+        philosophers[i].rightForkId = calculateRightForkId(philosophers[i].id);
         pthread_create(&threads[i], nullptr, think, &philosophers[i]);
     }
 
