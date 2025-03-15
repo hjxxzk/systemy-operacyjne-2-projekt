7c3ac6e9b81975cc9d5ac36c5c315795f5654a4a
agnieszka <273017@student.pwr.edu.pl>
2025-03-15 15:24:16 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Copyright 2025 Agnieszka
+
+#include <iostream>
+
+int main() {
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
+
